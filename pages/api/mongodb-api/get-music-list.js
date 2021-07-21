278c104f9d3f1976b2206cabdcbb9b6da0eef05e
Matheus Vancoll <matheu-s_andre@hotmail.com>
2021-07-21 10:37:36 -0300

    clear get music list
    remove "if" in method for "GET"

--- pages/api/mongodb-api/get-music-list.js
+++ pages/api/mongodb-api/get-music-list.js
@@ -2,23 +2,15 @@ import ConnectDatabase from '../../../utils/MongoConnection'
 
 export default async function GetMusicList(req, res) {
   
-  if (req.method === "GET") {
+  // Salva a data de hoje
+  const date = new Date()
+  const formatDate = Intl.DateTimeFormat("pt-BR", { dateStyle: "short" })
+  const currentDate = formatDate.format(date)
 
-    // Salva a data de hoje
-    const date = new Date()
-    const formatDate = Intl.DateTimeFormat("pt-BR", { dateStyle: "short" })
-    const currentDate = formatDate.format(date)
-
-    // Conecta ao DB e retorna a música de hojes
-    const { db } = await ConnectDatabase()
+  // Conecta ao DB e retorna a música de hojes
+  const { db } = await ConnectDatabase()
     
-    const musicListDay = await db.collection('music_day').findOne({dayMusic: currentDate})
+  const musicListDay = await db.collection('music_day').findOne({dayMusic: currentDate})
 
-    res.status(200).json({ music: musicListDay})
-
-    return musicListDay
-  } else {
-    res.status(400).json({ message: "Wrong request method"})
-  }
-
+  res.status(200).json({ music: musicListDay})
 }
